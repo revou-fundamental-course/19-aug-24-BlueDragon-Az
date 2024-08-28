59a0f5a0d8f4a45f2bd98e2859ae4567a4646599
unknown <putri.sofyar26@gmail.com>
2024-08-28 21:30:31 +0700

    tugas mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-//ini js//
+function replaceName() {
+    var name = prompt("Hey there! What's your name?", "");
+    document.getElementById("name").innerHTML = name
+}
+
+replaceName();
+
+function validateForm() {
+    const name = document.forms["message-form"]["fullName"].value;
+    const birthDate = document.forms["message-form"]["birthDate"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+    
+    if (name == "" || birthDate == "" || gender == "" || messages =="") {
+        alert("Please fill all field.");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, message) {
+    document.getElementById("senderFullName").innerHTML = name;
+    document.getElementById("senderBirthDate").innerHTML = birthDate;
+    document.getElementById("senderGender").innerHTML = gender;
+    document.getElementById("senderMessages").innerHTML = messages;
+}
